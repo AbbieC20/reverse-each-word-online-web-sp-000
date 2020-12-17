8d5f8066b055499ab4e604b41c64cd5034b9848c
remote-torrent-6747 <abbyc@sky.com>
2020-12-17 21:38:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(phrase)
   array_phrase = phrase.split(" ")
+  backwards_words = []
   array_phrase.each do |words|
-    backwards_words = []
     output = words.reverse
     backwards_words.push(output)
   end
